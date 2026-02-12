ab5c349412c3db972215304911a6c2706cdb416a
cauadebrito <you@example.com>
2026-02-12 18:41:43 -0300

    alterei algumas coisas tentando melhorar meu site. como usar overlay e js para abrir a aba de login, assim excluindo a necessidade de um html extra para aba de login.

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//seliciona os elementos 
+
+const btnLogin = document.getElementById("btnLogin")
+const overlay = document.getElementById("overlay")
+
+//Abrir o Login
+btnLogin.addEventListener("click", function(e){e.preventDefault(); 
+    overlay.classList.add("active");
+        document.body.style.overflow = "hidden";});
+
+//fechar o login
+overlay.addEventListener("click", function(e){
+    if(e.target === overlay){
+        overlay.classList.remove("active");
+        document.body.style.overflow = "auto";
+    }
+})
